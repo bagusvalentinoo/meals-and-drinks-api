d73df8043da838422c0f5e9631f57af79ff5a75d
Bagus Valentino <bagusvaalentino@gmail.com>
2024-07-25 10:46:45 +0700

    🎨 (style): Add task to clean up expired tokens.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { web } from '@app/web'
+import { scheduleJob } from 'node-schedule'
+import { cleanUpExpiredToken } from '@tasks/token/clean_up_token.task'
+
+scheduleJob('*/15 * * * *', cleanUpExpiredToken) // run every 15 minutes
 
 const port = Number(process.env.APP_PORT) || 8000
 const host = process.env.APP_HOST || 'localhost'
